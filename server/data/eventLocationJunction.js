ee09ad9529d82b27ec28fff2cb80ef6eefa63be2
FrenzyExists <frenzyexists@gmail.com>
2024-10-12 01:56:39 -0400

    added junction json

---
+++ server/data/eventLocationJunction.js
@@ -0,0 +1,79 @@
+const eventLocationJunction = [
+    {
+      "id": 1,
+      "event_id": 1,
+      "location_id": 1
+    },
+    {
+      "id": 2,
+      "event_id": 2,
+      "location_id": 1
+    },
+    {
+      "id": 3,
+      "event_id": 3,
+      "location_id": 2
+    },
+    {
+      "id": 4,
+      "event_id": 4,
+      "location_id": 2
+    },
+    {
+      "id": 5,
+      "event_id": 5,
+      "location_id": 3
+    },
+    {
+      "id": 6,
+      "event_id": 6,
+      "location_id": 3
+    },
+    {
+      "id": 7,
+      "event_id": 7,
+      "location_id": 3
+    },
+    {
+      "id": 8,
+      "event_id": 8,
+      "location_id": 3
+    },
+    {
+      "id": 9,
+      "event_id": 9,
+      "location_id": 3
+    },
+    {
+      "id": 10,
+      "event_id": 10,
+      "location_id": 3
+    },
+    {
+      "id": 11,
+      "event_id": 11,
+      "location_id": 1
+    },
+    {
+      "id": 12,
+      "event_id": 12,
+      "location_id": 1
+    },
+    {
+      "id": 13,
+      "event_id": 13,
+      "location_id": 4
+    },
+    {
+      "id": 14,
+      "event_id": 14,
+      "location_id": 4
+    },
+    {
+      "id": 15,
+      "event_id": 15,
+      "location_id": 4
+    }
+  ]
+
+  export default eventLocationJunction;
